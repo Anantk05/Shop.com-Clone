a42256fd6b743595ddb997a9985cfc5a8f2b023d
Dipak <itsdipakpawar4206@gmail.com>
2022-05-07 00:47:21 +0530

    Anant files added

---
+++ womenspage.js
@@ -0,0 +1,394 @@
+
+        // Women's Product data
+
+        var womensproductdata = [
+            {
+                name: "ADRIANA PAPELL Womens Black Sheer Short Sleeve Illusion Neckline Party T-Shirt",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1921266473.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "27.98",
+                offprice: "$109.00",
+                cashback: "+ $0.84 / 3%",
+            },
+            {
+                name: "Menber's Mark Women's High Rise Zip Pocket Pull On City Dress Pant",
+                imageurl: "https://img.shop.com/Image/290000/290600/290680/products/1920192288.jpg?size=400x400",
+                seller: "Sold by eWielessGear",
+                price: "17.49",
+                offprice: "",
+                cashback: "+ up to $1.35 / 6%",
+            },
+            {
+                name: "women's CHIN UP Tequila Sqad Racerback Tank Top",
+                imageurl: "https://img.shop.com/Image/290000/291200/291280/products/1924140609.jpg?size=400x400",
+                seller: "Sold by Fifth Sun",
+                price: "17.98",
+                offprice: "$109.00",
+                cashback: "+ $0.84 / 3%",
+            },
+            {
+                name: "BETSY & ADAM Womens Navy Embellished 3/4 Sleeve V Neck Full-Length Body Con Formal",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1925889420.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "91.98",
+                offprice: "$239.00",
+                cashback: "+ $2.76 / 3%",
+            },
+            {
+                name: "Member's Mark Women's Full Zip Favorite Soft Jacket w/Thumbholes",
+                imageurl: "https://img.shop.com/Image/290000/290600/290680/products/1920192201.jpg?size=400x400",
+                seller: "Sold by eWirelessGear",
+                price: "14.69",
+                offprice: "",
+                cashback: "+ up to $1.35 / 6%",
+            },
+            {
+                name: "ALEX EVENINGS Womens Navy Lace Formal Jacket",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1925885771.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "81.98",
+                offprice: "$199.00",
+                cashback: "+ $2.46 / 3%",
+            },
+            {
+                name: "KASPER Wmens Red Printed Knee Length A-Line Skirt",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1923609816.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "19.98",
+                offprice: "$79.00",
+                cashback: "+ $0.60 / 3%",
+            },
+            {
+                name: "MSK Womens Gray Embellished 3/4 Sleeve Point Collar Short shift Wear To Work Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1926448125.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "37.98",
+                offprice: "$69.00",
+                cashback: "+ $1.14 / 3%",
+            },
+            {
+                name: "Tahari Womens Paisley Midi Wrap Dress",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1919388686.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "29.99",
+                offprice: "$158.00",
+                cashback: "+ $0.60 / 2%",
+            },
+            {
+                name: "RALPH LAUREN Womens Silver Sleeveless Asymmetrical Neckline Maxi Sheath Evening Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1920679578.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "29.98",
+                offprice: "$230.00",
+                cashback: "+ $0.90 / 3%",
+            },
+            {
+                name: "WAYF Womens Ashley Ribbed Mini Halter Dress",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1924434117.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "18.99",
+                offprice: "",
+                cashback: "+ up to $0.50 / 2%"
+            },
+            {
+                name: "I-N-C Womens Floral Maxi Dress",
+                imageurl: "https://img.shop.com/Image/250000/253200/253251/products/1831026048.jpg?size=400x400",
+                seller: "Sold by Tags Weekly",
+                price: "61.14",
+                offprice: "",
+                cashback: "+ up to $1.38 / 2%",
+            },
+            {
+                name: "Lively Swirls - Pockets Top Quality Women's Fitness",
+                imageurl: "https://img.shop.com/Image/280000/288000/288080/products/1892927095.jpg?size=400x400",
+                seller: "Sold by NotJustLeggings",
+                price: "28.00",
+                offprice: "",
+                cashback: "+ $2.24 / 8%",
+            },
+            {
+                name: "Xscape Women's Dress Pink Size 12 A-Line Halter Jacquard Floral Skirt",
+                imageurl: "https://img.shop.com/Image/280000/289600/289605/products/1919583208.jpg?size=400x400",
+                seller: "Sold by Azura Runway",
+                price: "52.00",
+                offprice: "$183.00",
+                cashback: "+ $3.12 / 6%",
+            },
+            {
+                name: "Solid Black Two Large Pockets - Wide Band Top Quality Women's Fitness",
+                imageurl: "https://img.shop.com/Image/280000/288000/288080/products/1892927136.jpg?size=400x400",
+                seller: "Sold by NotJustLeggings",
+                price: "27.50",
+                offprice: "",
+                cashback: "+ up to $2.44 / 8%",
+            },
+            {
+                name: "B DARLIN Womens Red Sequined Solid Sleeveless Off Shoulder Maxi Fit + Flare Evening Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1924828993.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "26.98",
+                offprice: "$119.00",
+                cashback: "+ $0.81 / 3%",
+            },
+            {
+                name: "J Brand Womens Alana Denim Medium Wash Skinny Crop Jeans",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1893409378.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "24.99",
+                offprice: "$248.00",
+                cashback: "+ $0.50 / 2%",
+            },
+            {
+                name: "Women's Marvel Black Widow Stealth Barcode Graphic T-Shirt",
+                imageurl: "https://img.shop.com/Image/290000/291200/291280/products/1924143353.jpg?size=400x400",
+                seller: "Sold by Fifth Sun",
+                price: "27.98",
+                offprice: "$19.99",
+                cashback: "+ $1.08 / 6%",
+            },
+            {
+                name: "NAUTICA Womens Navy Belted Rain Coat",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1920267680.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "54.98",
+                offprice: "$150.00",
+                cashback: "+ $1.65 / 3%"
+            },
+            {
+                name: "Aqua Womens Glitter Halter Jumpsuit",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1871148694.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "14.65",
+                offprice: "",
+                cashback: "+ up to $0.30 / 2%",
+            },
+            {
+                name: "MSK Womens Green Zippered 3/4 Sleeve Collared Mini Shift Wear To Work Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1919087451.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "22.98",
+                offprice: "$69.00",
+                cashback: "+ $0.69 / 3%",
+            },
+            {
+                name: "MSK PETITES Womens Ivory Sheer Wide Leg Evening Pants",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1925888217.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "27.98",
+                offprice: "$49.00",
+                cashback: "+ $0.84 / 3%",
+            },
+            {
+                name: "MICHAEL KORS Womens Blue Lace Short Sleeve Crew Neck Short Shift Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1917716698.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "44.98",
+                offprice: "$155.00",
+                cashback: "+ $1.35 / 3%"
+            },
+            {
+                name: "J Brand Womens Tate Denim Light Wash Borfriend Jeans",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1892963527.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "21.99",
+                offprice: "$148.00",
+                cashback: "+ $0.44 / 2%",
+            },
+            {
+                name: "Adrianna Papell Womens Crop Square Neck Tank Top",
+                imageurl: "https://img.shop.com/Image/270000/273200/273200/products/1930607796.jpg?size=400x400",
+                seller: "Sold by BHFO",
+                price: "27.99",
+                offprice: "$99.00",
+                cashback: "+ $0.56 / 2%"
+            },
+            {
+                name: "CELEBRITY PINK Womens Pink Skinny Jeans",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1925887636.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "17.98",
+                offprice: "$44.00",
+                cashback: "+ $0.54 / 3%",
+            },
+            {
+                name: "M&M's Women's Mars Candy Chocolate M and M Spiral Tie-Die GIrls T-Shirt",
+                imageurl: "https://img.shop.com/Image/250000/254500/254570/products/1896906353.jpg?size=400x400",
+                seller: "Sold by Seven Times Six",
+                price: "16.95",
+                offprice: "",
+                cashback: "+ up to $0.66 / 2%"
+            },
+            {
+                name: "ALFANI Womens Black Pocketed Wide Leg Wear To Work Pants",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1920267848.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "15.98",
+                offprice: "$69.50",
+                cashback: "+ $0.48 / 3%"
+            },
+            {
+                name: "Michael Kors Womens Handkerkchief Print Casual Cropped Pants",
+                imageurl:"https://img.shop.com/Image/250000/253200/253251/products/1865990768.jpg?size=400x400",
+                seller: "Sold by Tags Weekly",
+                price: "58.82",
+                offprice: "$88.00",
+                cashback: "+ $1.18 / 2%",
+            },
+            {
+                name: "Seven7 Women's 4-Way Stretch Skinny Leg Pull On Ponte Pant Legging",
+                imageurl: "https://img.shop.com/Image/290000/290600/290680/products/1920192103.jpg?size=400x400",
+                seller: "Sold by eWirelessGear",
+                price: "19.99",
+                offprice: "",
+                cashback: "+ up to $1.33 / 6%",
+            },
+            {
+                name: "SPEECHLESS Womens Green Embroidered Sleeveless Halter Full-Length Prom Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1921134831.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "37.98",
+                offprice: "$149.00",
+                cashback: "+ $1.14 / 3%",
+            },
+            {
+                name: "Bar III Womens Knit Casual Wide Leg Pants",
+                imageurl: "https://img.shop.com/Image/250000/253200/253251/products/1788900538.jpg?size=400x400",
+                seller: "Sold by Tags Weekly",
+                price: "$34.14",
+                offprice: "",
+                cashback: "+ up to $0.76 / 2%",
+            },
+            {
+                name: "Menber's Mark Women's Moisture Wicking Work It Out Short Sleeve Active T-Shirt",
+                imageurl: "https://img.shop.com/Image/290000/290600/290680/products/1920192322.jpg?size=400x400",
+                seller: "Solf by eWirelessGear",
+                price: "$13.49",
+                offprice: "",
+                cashback: "+ up to $1.02 / 6%",
+            },
+            {
+                name: "Women's CHIN UP Had Me at Daiquiri Racerback Tank Top",
+                imageurl: "https://img.shop.com/Image/290000/291200/291280/products/1924134286.jpg?size=400x400",
+                seller: "Sold by Fifth Sun",
+                price: "17.98",
+                offprice: "$29.99",
+                cashback: "+ $1.08 / 6%",
+            },
+            {
+                name: "AQUA Womens Black Slitted Darted Cap Sleeve Off Shoulder Full-Length Prom Dress",
+                imageurl: "https://img.shop.com/Image/270000/278100/278132/products/1917711633.jpg?size=400x400",
+                seller: "Sold by BOBBI + BRICKA",
+                price: "39.98",
+                offprice: "$268.00",
+                cashback: "+ $1.20 / 3%",
+            }
+        ];
+
+displaymens(womensproductdata)
+
+function displaymens(data){
+
+    document.querySelector("#products_container").innerHTML=null
+    data.forEach(function(el){
+
+        var product_box = document.createElement("div")
+
+        var image = document.createElement("img")
+        image.setAttribute("src",el.imageurl)
+        var name = document.createElement("div")
+        name.classList.add("product_name")
+        name.innerText = el.name
+        
+
+        var cartimg = document.createElement("img")
+        cartimg.setAttribute("src","https://img.shop.com/Image/resources/images/onecart-icon.svg")
+        var seller = document.createElement("p")
+        seller.innerText = el.seller
+
+        var offprice_price = document.createElement("p")
+        var offprice = document.createElement("span")
+        offprice.innerText = el.offprice
+        var dollarsign = document.createElement("span")
+        dollarsign.innerText = "$"
+        dollarsign.style.fontWeight="bold"
+        dollarsign.style.fontSize="15px"
+        var price = document.createElement("span")
+        price.classList.add("price")
+        price.innerText = el.price
+        offprice_price.append(offprice,dollarsign,price)
+
+        var cashback = document.createElement("p")
+        cashback.classList.add("cashback_text")
+        cashback.innerText = el.cashback
+        var cashbacksign = document.createElement("img")
+        cashbacksign.setAttribute("src","https://img.shop.com/Image/resources/images/cashback-icon.svg")
+        var cashbacktext = document.createElement("span")
+        cashbacktext.innerText = "Cashback"
+
+        var buttonbox = document.createElement("div")
+        buttonbox.classList.add("products_button")
+        var button1 = document.createElement("div")
+        button1.innerText = "Add To Cart"
+        button1.addEventListener("click",function(){
+            addtocart_fun(el)
+        })
+        var button2 = document.createElement("div")
+        button2.id= "buying_button"
+        button2.innerText = "Buying Options"
+        buttonbox.append(button1,button2)
+
+        product_box.append(image,name,cartimg,seller,offprice_price,cashback,cashbacksign,cashbacktext,buttonbox)
+        document.querySelector("#products_container").append(product_box)            
+    })
+}
+        // Add To Cart Function
+    var cartsdata = JSON.parse(localStorage.getItem("cartsdata")) || []
+function addtocart_fun(el){
+
+    if(cartsdata!=false){
+        
+        var check = false
+        for(var i=0;i<cartsdata.length;i++){
+            if(el.name==cartsdata[i].name){
+                check = true
+                break;
+            }
+        }
+        if(check){
+            alert("This item is already added!")
+        }
+        else{
+            cartsdata.push(el)
+            localStorage.setItem("cartsdata",JSON.stringify(cartsdata))
+            alert("Item added successfully. ")
+        }
+    }
+    else{
+        cartsdata.push(el)
+        localStorage.setItem("cartsdata",JSON.stringify(cartsdata))
+        alert("Item added successfully. ")
+    }
+    
+}
+
+// sorting by Price Function
+
+document.querySelector("#for_sorting").addEventListener("change",sorting_fun)
+
+function sorting_fun(){
+
+    var sorting_value=document.querySelector("#for_sorting").value
+
+    if(sorting_value=="price_high-low"){
+        womensproductdata.sort(function(a,b){
+            return b.price-a.price
+        })
+        displaymens(womensproductdata)
+    }
+    if(sorting_value=="price_low-high"){
+        womensproductdata.sort(function(a,b){
+            return a.price-b.price
+        })
+        displaymens(womensproductdata)
+    }
+}
