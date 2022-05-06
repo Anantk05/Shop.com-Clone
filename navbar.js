566ea2df3b7fbd28e794d703b9110e10f82b5785
Dipak <itsdipakpawar4206@gmail.com>
2022-05-07 00:15:00 +0530

    navbar content added

--- navbar.js
+++ navbar.js
@@ -3,3 +3,21 @@ var accou=document.querySelector("#account")
 accou.addEventListener("click",function(){
       window.location.href="Singinforhomepage.html"
 })
+
+document.querySelector("select").addEventListener("change",selectfunction)
+function selectfunction(){
+      event.preventDefault()
+var result=document.querySelector("select").value
+console.log(result)
+      if(result=="Men"){
+            window.location.href="menspage.html"
+      }
+      else if(result=="Women")
+      {
+            window.location.href="womenspage.html"
+      }
+      else{
+            window.location.href="navbar.html"
+      }
+     
+}
